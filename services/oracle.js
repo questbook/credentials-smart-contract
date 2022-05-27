bd57ba3641c33c65bd381639e5d0c902fe3549c0
justcharlz <charlie4biz@gmail.com>
2022-05-27 17:38:01 +0100

    oracle added alert

--- services/oracle.js
+++ services/oracle.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+let alert = require('alert'); 
 const {getUsersInList} = require("./usersApi");
 const { ethers } = require("hardhat");
 const {oracleContractAbi, communicationAbi, governanceAbi, workAbi} = require("./abis");
@@ -8,27 +9,23 @@ const provider = new ethers.providers.InfuraProvider("rinkeby", {projectId: proc
 const abiCoder = new ethers.utils.AbiCoder();
 // let wallet = new ethers.Wallet(privateKey, provider);
 const contract = new ethers.Contract(oracleAddr, oracleContractAbi, provider);
-
 let paramArray = [];
 
 exports.oracle = async () =>{ 
-
 contract.on("RequestCredentialsCallback", async (list_id, msg_sender, contractAddress, funcName, msg_data)=>{
-   
     const bytecode = await provider.getCode(contractAddress);
     paramArray = await abiCoder.decode(['string','string'], ethers.utils.hexDataSlice(msg_data, 4));
-
     const forAuthUsers = funcName+ "_authorized";
     const forUnAuthUsers = funcName + "_unauthorized";
-    const callFunction = new ethers.Contract(contractAddress, bytecode);
+    // const callFunction = new ethers.Contract(contractAddress, bytecode, provider);
 
-    const users = await getUsersInList(list_id);
-    if(msg_sender in users){
-        alert('User is authorized \n','Function Name:', forAuthUsers,'\n Contract Address:', contractAddress,'\n Data:', paramArray);
+    const users = await getUsersInList(paramArray[1]);
+    if(paramArray[0] in users){
+        alert('User is authorized \n  Function Name: '+forUnAuthUsers+' \n Contract Address: '+contractAddress+'\n List_id -'+ paramArray[1]+'\n Param -'+ paramArray[0]);
     //   const proof = []; //we'll create proofs later
     //   await callFunction.forAuthUsers(params, proof); // need to use a signer for this
     }else{
-        alert('User is unauthorized \n','Function Name:', forAuthUsers,'\n Contract Address:', contractAddress)
+        alert('User is unauthorized \n  Function Name: '+forUnAuthUsers+' \n Contract Address: '+contractAddress+'\n List_id:'+ paramArray[1]+'\n Param:'+ paramArray[0]);
         // await callFunction.forUnAuthUsers(params);
     }
 
