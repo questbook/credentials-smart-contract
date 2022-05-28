49778d63ebd6d627f6ecad3c3257f6c837058538
justcharlz <charlie4biz@gmail.com>
2022-05-28 16:44:48 +0100

    oracle search parameter added to api list query

--- services/oracle.js
+++ services/oracle.js
@@ -22,8 +22,8 @@ contract.on("RequestCredentialsCallback", async (list_id, msg_sender, contractAd
     // const callFunction = new ethers.Contract(contractAddress, bytecode, provider);
 
     const users = await getUsersInList(paramArray[1]);
-    
-    if(paramArray[0] in users){
+    const findUser = users.find(user => user.id === paramArray[0]);
+    if(findUser){
         response = 'User is authorized \n  Function Name: '+forUnAuthUsers+' \n Contract Address: '+contractAddress+'\n List_id -'+ paramArray[1]+'\n Param -'+ paramArray[0] ;
     //   const proof = []; //we'll create proofs later
     //   await callFunction.forAuthUsers(params, proof); // need to use a signer for this
--- services/usersApi.js
+++ services/usersApi.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const axios = require("axios");
 
 exports.getUsersInList = async function(list_id) {
-    const url = `${process.env.API_URL}/lists/${list_id}/users`;
+    const url = `${process.env.API_URL}/lists/${list_id}`;
     const response = await axios(url)
     .then(res => res.json())
     .catch(err => err);
@@ -18,7 +18,7 @@ exports.getUser = async function(user_id) {
 }
 
 exports.checkUser = async function(user_id, list_id) {
-    const url = `${process.env.API_URL}/lists/${user_id}/users/${list_id}`;
+    const url = `${process.env.API_URL}/users/${user_id}/lists/${list_id}`;
     const response = await fetch(url);
     const user = await response.json();
     return user;
